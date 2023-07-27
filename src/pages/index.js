4159439d069b32ca50da8d1d8670c375efe267b5
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-07-28 02:02:50 +0600

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
+import Head from 'next/head';
 import RootLayout from "./components/Layouts/RootLayout";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>Home page</h2>
+        <>
+     <Head>
+       <title>PC_Builder</title>
+       <meta
+         name="description"
+         content="This is PC_Builder website made by next-js"
+       />
+       <meta name="viewport" content="width=device-width, initial-scale=1" />
+       <link rel="icon" href="/favicon.ico" />
+     </Head>
+   </>
     </div>
   );
 };
