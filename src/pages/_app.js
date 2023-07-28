885af94eff5a2253f84cff238a7f8b574e8a41d7
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-07-29 04:27:49 +0600

    add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
-import store from '@/redux/store';
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react';
-import { Provider } from 'react-redux';
+
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
